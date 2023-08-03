4a382694e85ed1e0def527bc10f57d935331f31a
TheJackal03 <gregorytsak3@gmail.com>
2023-08-03 17:04:04 +0300

    changes on database.py

--- app/database.py
+++ app/database.py
@@ -1,9 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from fastapi import Request
-import psycopg2, time
-from psycopg2.extras import RealDictCursor
 from .config import settings
 
 SQL_ALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
